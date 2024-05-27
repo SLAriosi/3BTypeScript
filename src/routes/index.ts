f5299485dc41e39bcd297415a2c494d5be9049fd
UniALFA <unialfa@MACMINI-11.local>
2024-05-27 20:15:17 -0300

    Adicionando nova rota ao arquivo index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import usuarioRouter from './usuario'
 import categoriaRouter from './categoria'
+import sessionRouter from './session'
 
 const routes = Router()
 
 routes.use('/usuarios', usuarioRouter)
 routes.use('/categorias', categoriaRouter)
+routes.use('/', sessionRouter)
 
 export default routes
