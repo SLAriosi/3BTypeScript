2a25bb2571d8e39573dea18adfd7501f2bcdf4e1
UniALFA <unialfa@MACMINI-11.local>
2024-06-03 21:40:23 -0300

    Aula dia 03 de Junho de 2024 com o professor Chinês

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,12 @@ import { Router } from 'express'
 import usuarioRouter from './usuario'
 import categoriaRouter from './categoria'
 import sessionRouter from './session'
+import autenticacao from '../middlewares/autenticacao'
 
 const routes = Router()
 
 routes.use('/usuarios', usuarioRouter)
-routes.use('/categorias', categoriaRouter)
+routes.use('/categorias', autenticacao, categoriaRouter)
 routes.use('/session', sessionRouter)
 
 export default routes
